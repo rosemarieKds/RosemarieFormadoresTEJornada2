c6c2162068c67d1492b4d6a45b7bdc416e0e5491
rosemarieKds <145386564+rosemarieKds@users.noreply.github.com>
2025-07-01 14:40:56 -0300

    .

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ function inserirNome () {
     console.log(elemento);
     elemento.textContent = nomeUsuario;
 }
+
 inserirNome();
+
 let linguagens = [ "JavaScript", "Pyton", "C++"]
 console.log (linguagens[0])
 console.log (linguagens[1])
 console.log(linguagens[2])
+
+const lista = document.querySelector ("#lista");
+lista.textContent = (linguagens[0]);
+lista.textContent = (linguagens[1]);
+lista.textContent = (linguagens[2]);
