c8d1b6fa427feebdc4104415ebebc05fecf93e59
rosemarieKds <145386564+rosemarieKds@users.noreply.github.com>
2025-07-01 14:29:50 -0300

    adição de códigos Js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function inserirNome () {
     let nomeUsuario = prompt ("Qual o seu nome?");
     let elemento = document.querySelector("#nome-usuario");
+    console.log(elemento);
     elemento.textContent = nomeUsuario;
 }
 inserirNome();
+let linguagens = [ "JavaScript", "Pyton", "C++"]
+console.log (linguagens[0])
+console.log (linguagens[1])
+console.log(linguagens[2])
